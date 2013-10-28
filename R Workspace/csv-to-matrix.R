be1324dc50f0d5a57837e911eb39793409e44d9d
David Sherlock <davidsherlock163@hotmail.com>
2013-10-28 09:56:13 +0000

    git ignire

--- R Workspace/csv-to-matrix.R
+++
@@ -1,8 +0,0 @@
-df<-read.csv("topics_posts.csv" , header=T, sep=",")
-M = as.matrix( table(df) )
-Mrow = M %*% t(M)
-#Mcol = t(M) %*% M
-iMrow = graph.adjacency(Mrow, mode = "undirected")
-E(iMrow)$weight <- count.multiple(iMrow)
-iMrow <- simplify(iMrow)
-write.graph(iMrow, file="graph.graphml", format="graphml");
