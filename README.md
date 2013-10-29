f7e0013d6c31abe0146eef566d694a1707204e12
David Sherlock <davidsherlock163@hotmail.com>
2013-10-29 10:30:57 +0000

    reorganise

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+Setup
+---------
+Set settings in /settings
+Create symbolic for mysql if needed, ln -s /Applications/MAMP/tmp/mysql/mysql.sock /tmp/mysql.sock
+
 
 Network Tools
 -----------
@@ -7,8 +12,11 @@ Text Tools
 
 Outputs
 ----------
+Gephi files, plots, etc. Should be moved to each scripts folder
+
 
 Notes
 ---------
+notes I've bunged in word, powerpoint or whatever. Might be useful
 
 
---
+++ network_tools/centrality_measures.R
@@ -0,0 +1,42 @@
+
+#TODO: use igraph to produce a pretty report about IDS and who is important. Check out Adams Knitr work
+#TODO create a graph with attributes
+
+library("RMySQL")
+library("igraph")
+
+top_degrees = list()
+top_bm = list()
+
+
+for (i in settings.databases[,1]) {
+  
+  db<-i
+  print(db)
+  
+  mydb = dbConnect(MySQL(), user=settings.mysqlusername, password=settings.mysqlpassword, dbname=db, host='localhost')
+  query<-paste('select forum_user_id, post_id from `forum_comments`')
+  results.graph = dbGetQuery(mydb,query)
+  dbDisconnect(mydb)
+  
+  M = as.matrix( table(results.graph) )
+  Mrow = M %*% t(M)
+  #Mcol = t(M) %*% M
+  iMrow = graph.adjacency(Mrow, mode = "undirected")
+  E(iMrow)$weight <- count.multiple(iMrow) # Remove multiple edges but keep multiplicity
+  iMrow <- simplify(iMrow) #dont care if we have multiple edges
+  
+  #lets do some community detection
+  #using Clique percolation, implementation taken from igraph team (although it's not in igraph.) This seems a little rusty, perhaps we should stick to gephi?
+  
+  #lets find the 3 classic centrality measures
+  
+  betweeness_measures<-sort(betweenness(iMrow), decreasing = TRUE)
+  bm_top20 <- betweeness_measures[1:20]
+  degree_measures<-sort(degree(iMrow), decreasing = TRUE)
+  de_top20 <- degree_measures[1:20]
+  
+  top_bm[[i]]<-bm_top20
+  top_degrees[[i]]<-de_top20
+
+  }
